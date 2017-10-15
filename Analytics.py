b7e77fe25cdf99cfd2223ef8d6844230b16bb36d
duleignjatovic995 <dusan.ignjatovic@hotmail.com>
2017-10-15 05:48:23 +0200

    updated analytics

---
+++ Analytics.py
@@ -0,0 +1,150 @@
+import pandas as pd
+import numpy as np
+import util
+from matplotlib import pyplot as plt
+
+from sklearn.pipeline import make_pipeline
+from sklearn.feature_selection import SelectKBest
+from sklearn.model_selection import StratifiedKFold
+from sklearn.grid_search import GridSearchCV
+from sklearn.ensemble.gradient_boosting import GradientBoostingClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
+
+
+# FEATURE ENGINEERING
+
+# For feature engineering we are going to merge train and test set.
+# The reason we are doing it is because test set doesn't have Survived
+# target feature, and we want to sync our train and test set
+# with all newly added features.
+
+# Combine train and test set
+combined0 = util.get_combined_data()
+# print(combined0.shape)
+# print(combined0.head())
+
+# Creating new feature based on title in Name value
+combined1 = util.get_titles(combined0)
+# print(combined1.head())  # We can see newly added feature as the last column
+
+
+# We've seen that there is 177 missing values in age variable.
+# Simply filling it with mean or median isn't the best possible solution.
+
+# Group dataset by Sex, Pclass and Title and
+# fill the missing values based on the groups mean
+combined2 = util.process_age(combined1)
+# print(combined2.head())
+
+
+# Create dummy variables based on titles and drop alredy used names variable
+combined3 = util.process_names(combined2)
+# print(combined3.head())
+
+
+# Replace missing fare values by mean
+combined4 = util.process_fares(combined3)
+# print(combined4.head())
+
+
+# Replace missing Embarked value with most frequent
+combined5 = util.process_embarked(combined4)
+# print(combined5.head())
+
+
+# Create dummy variable with Cabin feature
+combined6 = util.process_cabin(combined5)
+# print(combined6.head())
+
+
+# 1 if male 0 if female
+combined7 = util.process_sex(combined6)
+# print(combined7.head())
+
+
+# Create dummy variables based on passenger class
+combined8 = util.process_pclass(combined7)
+# print(combined8.head())
+
+
+# Create dummy variables with ticket prefixes
+combined9 = util.process_ticket(combined8)
+# print(combined9.head())
+
+
+# Create new variables based on family size
+combined = util.process_family(combined9)
+# print(combined.shape)
+# print(combined.info())
+# print(combined.head())
+
+
+# Recover dataset
+train0 = pd.read_csv('train.csv')
+
+targets = train0.Survived  # Survived
+train = combined.head(891)  # Initial training set
+test = combined.iloc[891:]  # Initial test set
+
+
+# FEATURE SELECTION
+
+# Tree based models can be used to
+clf = RandomForestClassifier(n_estimators=50, max_features='sqrt')
+clf = clf.fit(train, targets)
+
+# Plot feature importance
+features = pd.DataFrame()
+features['feature'] = train.columns
+features['importance'] = clf.feature_importances_
+features.sort_values(by=['importance'], ascending=True, inplace=True)
+features.set_index('feature', inplace=True)
+features.plot(kind='barh', figsize=(20, 20))
+plt.show()
+
+
+model = SelectFromModel(clf, prefit=True)
+train_reduced = model.transform(train)
+# print(train_reduced.shape)
+
+
+test_reduced = model.transform(test)
+# print(test_reduced.shape)
+
+
+# MODELING AND TUNING
+
+# Tuning Random Forests with grid search
+parameter_grid = {
+    'max_depth': [4, 6, 8],
+    'n_estimators': [50, 10],
+    'max_features': ['sqrt', 'auto', 'log2'],
+    'min_samples_split': [2, 3, 10],
+    'min_samples_leaf': [1, 3, 10],
+    'bootstrap': [True, False],
+}
+forest = RandomForestClassifier()
+cross_validation = StratifiedKFold(targets, n_folds=5)
+
+grid_search = GridSearchCV(forest,
+                           scoring='accuracy',
+                           param_grid=parameter_grid,
+                           cv=cross_validation)
+
+grid_search.fit(train, targets)
+model = grid_search
+parameters = grid_search.best_params_
+
+print('Best score: {}'.format(grid_search.best_score_))
+print('Best parameters: {}'.format(grid_search.best_params_))
+
+# parameters = {'bootstrap': False, 'min_samples_leaf': 3, 'n_estimators': 50,
+#               'min_samples_split': 10, 'max_features': 'sqrt', 'max_depth': 6}
+#
+# model = RandomForestClassifier(**parameters)
+# model.fit(train, targets)
+
+# Print accuracy score
+print(util.compute_score(model, train, targets, scoring='accuracy'))
